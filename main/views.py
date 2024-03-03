d911c9315b78e50dd916188df886df428accf573
Igor <igorgood2013@mail.com>
2024-03-03 20:25:12 +0200

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -3,10 +3,21 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        "title": 'HOME',
+        "title": 'Mel.ua - Головна сторінка',
         "content": 'страница HOME'
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return HttpResponse('About page ')
+    context = {
+        "title": 'Mel.ua - Про Нас',
+        "content": "doskalike.com.ua – надійний виробник якісної рекламної "
+                   "продукції України. Розвиток кав'ярні, ресторану, бару,"
+                   " нічного клубу, невеликої приватної пекарні неможливий без "
+                   "грамотної побудови маркетингової стратегії. "
+                   "Інтернет-магазин «DOSKALIKE» виробляє та реалізує "
+                   "найефективніші інструменти реклами для подібного бізнесу – "
+                   "крейдяні штендери, дошки меню, лайтбокси, вивіски, підставки, "
+                   "та багато іншої продукції для внутрішньої та зовнішьої реклами"
+    }
+    return render(request, 'main/about.html', context)
